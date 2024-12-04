4a893ea8c6b807368fa4a8ee76ec8187a445ef86
BugHunter-X <askzenai@gmail.com>
2024-12-04 18:18:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,8 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = 0;
+for (int i = 0; i <= numbers.length; i++) { //Error: accessing index out of bounds
+  sum += numbers[i];
+}
+print(sum);
+```
